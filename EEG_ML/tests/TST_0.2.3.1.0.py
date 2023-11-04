b0acfa485d467a6bfb1fc9545f5f4418ab3e3e06
kguilly <guillotkaleb01@gmail.com>
2023-11-04 16:04:57 -0500

    test 0.2.3.1.0

--- EEG_ML/tests/TST_0.2.3.1.0.py
+++ EEG_ML/tests/TST_0.2.3.1.0.py
@@ -5,6 +5,9 @@ for the brain controlled wheelchair
 This test compares a different combination of hyper parameters
 for each run.
 
+This is a brute force method. Due to time constraints, a random three
+subjects were chosen: 7, 13, 84
+
 Every subject from the open source dataset was used, and their accuracy
 scores for the model were kept in a pandas df. the scores for each method
 were then averaged.
@@ -20,6 +23,7 @@ https://physionet.org/content/eegmmidb/1.0.0/
 from the PhysioToolkit Software:
 https://archive.physionet.org/physiotools/
 '''
+import csv
 
 import pandas as pd
 import numpy as np
@@ -40,6 +44,56 @@ def add_to_path(directory):
 with add_to_path('/home/kaleb/Documents/GitHub/BrainControlledWheelchair/EEG_ML'):
     from EEGModels import EEGNet
 
+def get_model_acc(X, Y, dropoutRate, kernels, kernLength,
+                  F1, D, F2, batch_size):
+    half = int(len(X) / 2)
+    quarter = int(half / 2)
+    three_fourths = half + quarter
+
+    X_train = X[:half, :, :]
+    X_validate = X[half: three_fourths, :, :]
+    X_test = X[three_fourths:, :, :]
+
+    y_train = Y[:half]
+    y_validate = Y[half:three_fourths]
+    y_test = Y[three_fourths:]
+
+    # convert labels to one-hot encoding
+    y_train = np_utils.to_categorical([x - 1 for x in y_train])
+    y_validate = np_utils.to_categorical([x - 1 for x in y_validate])
+    y_test = np_utils.to_categorical([x - 1 for x in y_test])
+
+    # convert data to NHWC (trials, channels, samples, kernels) format
+    X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], kernels)
+    X_validate = X_validate.reshape(X_validate.shape[0], X_validate.shape[1], X_validate.shape[2], kernels)
+    X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], kernels)
+
+    print('x_train shape: ', X_train.shape, '\ny_train shape: ', y_train.shape)
+    ################################################################
+    ## Call EEGNet
+    model = EEGNet(nb_classes=5, Chans=X_train.shape[1], Samples=X_train.shape[2],
+                   dropoutRate=dropoutRate, kernLength=kernLength, F1=F1, D=D, F2=F2,
+                   dropoutType='Dropout')
+
+    # compile the model and set the optimizers
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+
+    # set a valid path for your system to record model checkpoints
+    chkpt_filepath = '/home/kaleb/tmp/checkpoint.h5'
+    checkpointer = ModelCheckpoint(filepath=chkpt_filepath, verbose=1,
+                                   save_best_only=True)
+    # the syntax is {class_1:weight_1, class_2:weight_2,...}. Here just setting
+    # the weights all to be 1
+    class_weights = {0: 1, 1: 1, 2: 1, 3: 1, 4: 1}
+    model.fit(X_train, y_train, batch_size=batch_size, epochs=30,
+              verbose=2, validation_data=(X_validate, y_validate),
+              callbacks=[checkpointer], class_weight=class_weights)
+    probs = model.predict(X_test)
+    preds = probs.argmax(axis=-1)
+    acc = np.mean(preds == y_test.argmax(axis=-1))
+    return acc
+
 hyperparameter_map = {
     'dropoutRate' : [0.4, 0.5, 0.6],
     'kernels' : [1, 2, 3],
@@ -48,8 +102,52 @@ hyperparameter_map = {
     'D' : [1, 2, 3],
     'F2' : [8, 16, 32],
     'batch_size' : [8, 16, 32]
-    'epochs' : [15, 30, 60]
 }
+
+output_path = '/home/kaleb/Documents/GitHub/BrainControlledWheelchair/EEG_ML/tests/test_data/0.2.3.1.0.csv'
+col_names = ['Subject', 'HyperParams', 'Acc Score']
+with open(output_path, 'w', newline='') as file:
+    csv_writer = csv.DictWriter(file, fieldnames=col_names)
+    csv_writer.writeheader()
+
+import itertools
+combinations = list(itertools.product(*(hyperparameter_map[param] for param in hyperparameter_map)))
+random_subjects = [7, 13, 84]
+
+for subject in random_subjects:
+    for combination in combinations:
+        try:
+            X, Y = ref.reader(passed_path='/home/kaleb/Documents/eeg_dataset/files/', patient_num=subject)
+            acc = get_model_acc(X, Y, combination[0], combination[1], combination[2],
+                                combination[3], combination[4], combination[5], combination[6])
+
+            # write that acc out to file
+            with open(output_path, 'a', newline='') as file:
+                csv_writer = csv.DictWriter(file, fieldnames=col_names)
+                data = {
+                    'Subject' : subject,
+                    'HyperParams' : combination,
+                    'Acc Score' : acc
+                }
+                csv_writer.writerow(data)
+        except:
+            continue
+
+# after all have finished, find the max vals for each subject and append to end
+df = pd.read_csv(output_path)
+max_acc_rows = df.loc[df.groupby('Subject')['Acc Score'].idxmax()]
+
+new_rows = []
+for index, row in max_acc_rows.iterrows():
+    new_row = {
+        'Subject' : f'Max for Subject {row["Subject"]}',
+        'HyperParams' : row['HyperParams'],
+        'Acc Score' : row['Acc Score']
+    }
+    new_rows.append(new_row)
+new_row_df = pd.DataFrame(new_rows)
+df = pd.concat([df, new_row_df], ignore_index=True)
+df.to_csv(output_path, index=False)
 
 
 
--- EEG_ML/tests/scratch.py
+++ EEG_ML/tests/scratch.py
@@ -1,98 +1,42 @@
-import numpy as np
-import os 
-import pyedflib
+import keras
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Conv1D, GlobalMaxPooling1D
 
+# Define your model architecture
+def create_model(dropoutRate=0.5, kernels=1, kernLength=32, F1=8, D=2, F2=16, dropoutType='Dropout', loss='categorical_crossentropy', optimizer='adam'):
+    model = Sequential()
+    model.add(Conv1D(filters=F1, kernel_size=kernLength, activation='relu', input_shape=(input_shape)))
+    model.add(Dropout(dropoutRate, noise_shape=None, seed=None))
+    model.add(Conv1D(filters=F2, kernel_size=kernLength, activation='relu'))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(output_shape, activation='softmax'))
 
-data_path = "/home/kaleb/Documents/eeg_dataset/files/S001/S001R10.edf" # grabbing one subject
-# num = int(data_path[-5:-7])
-num = int(data_path[-5] + data_path[-6])
-other = int(data_path[-6] + data_path[-5])
-print(num)
-print(other)
+    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
+    return model
 
+# Define hyperparameters grid
+param_grid = {
+    'dropoutRate': [0.5, 0.6],  # Example values, you can expand this list
+    'kernels': [1, 2],
+    'kernLength': [32, 64],
+    'F1': [8, 16],
+    'D': [2, 4],
+    'F2': [16, 32],
+    'dropoutType': ['Dropout', 'SpatialDropout1D'],
+    'loss': ['categorical_crossentropy'],
+    'optimizer': ['adam'],
+    'batch_size': [16],
+    'epochs': [30]
+}
 
-##########################################################
-print("\n\n###################################\n")
-print("Reading from S001R10")
-edf_data = pyedflib.EdfReader(data_path)
-arr = edf_data.readSignal(1)
+# Create a model
+model = KerasClassifier(build_fn=create_model, input_shape=(your_input_shape), output_shape=(your_output_shape), verbose=0)
+model = keras.models.Model.
+# Perform grid search
+grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=2)  # You can adjust the number of cross-validation folds
+grid_result = grid.fit(X_train, y_train)  # X_train and y_train are your training data and labels
 
-edf_data.close()
-print("Shape: ", arr.shape)
-
-## visualize the data
-import matplotlib.pyplot as plt
-
-time = np.arange(len(arr))
-plt.figure(figsize=(10,4))
-plt.plot(time, arr, lw=0.5)
-plt.title('EEG data')
-plt.grid(True)
-# plt.show()
-
-###############################################
-### READING THE .EVENT FILES
-print("\n\n###################################\n")
-print("READING THE .EVENT FILES")
-import mne 
-
-file = '/home/kaleb/Documents/eeg_dataset/files/S001/S001R14.edf.event'
-# annotations = mne.read_annotations(file)
-# print(annotations)
-
-# Initialize lists to store annotation information
-onsets = []
-durations = []
-descriptions = []
-
-# Read the file line by line and parse the annotations
-
-with open(file, 'rb') as file:
-    for line in file:
-        print(line)
-    
-        # parts = line.strip().split()  # Split each line into parts
-        # if len(parts) >= 3:
-        #     onset = float(parts[0])  # Parse onset time
-        #     duration = float(parts[1])  # Parse duration
-        #     description = parts[2]  # Parse annotation description
-        #     onsets.append(onset)
-        #     durations.append(duration)
-        #     descriptions.append(description)
-
-
-# Create an Annotations object manually
-# annotations = mne.Annotations(onset=onsets, duration=durations, description=descriptions)
-
-# Now you can work with the annotations object
-# print(annotations)
-
-
-########################################################
-print("\n\n###################################\n")
-print("READING THE ENTIRE FILE")
-import pyedflib
-import numpy as np
-
-file = '/home/kaleb/Documents/eeg_dataset/files/S001/S001R14.edf'
-
-edf_data = pyedflib.EdfReader(file)
-
-# print all the attributes
-print(edf_data.file_info)
-print("file duration: ", edf_data.getFileDuration())
-print("num samples: ", edf_data.getNSamples())
-
-annotation = edf_data.readAnnotations()
-
-eeg_data = []
-for channel in range(64):
-    arr = edf_data.readSignal(channel)
-    eeg_data.append(arr)
-
-print(" ")
-eeg_data = np.stack(eeg_data)
-annotation = np.stack(annotation)
-print(eeg_data.shape)
-
-print("annotations: ", annotation)
+# Print the best parameters and corresponding accuracy
+print("Best parameters found: ", grid_result.best_params_)
+print("Best accuracy found: ", grid_result.best_score_)
